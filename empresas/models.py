38e74a368e6e1b645f145c29f085be6669fd8890
Marcia <marcia@hotmail.com>
2020-01-16 16:39:02 -0300

    Conflito de merge

--- empresas/models.py
+++ empresas/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
 # Create your models here.
+class Empresa(models.Model):
+  GENEROS = (
+    ('M', 'Masculino'),
+    ('F', 'Feminino'),
+    ('NB', 'Não-binário'),
+    ('O', 'Outros'),
+  )
+  nome = models.CharField(max_length=255, verbose_name='Nome')
+  cpf = models.CharField(max_length=255, verbose_name='CPF')
+  email = models.EmailField(max_length=255, verbose_name='E-mail')
+  telefone = models.CharField(max_length=255, verbose_name='Telefone')
+  genero = models.CharField(max_length=255, verbose_name='Gênero', choices=GENEROS)
+  ativo = models.BooleanField(default=True)
+
+  def __str__(self):
+    return self.nome
+nome:
+endereco:
+cnpj:
+telefone:
+email:
+quantidade_de_vagas:
+cargo:
+descricao_da_vaga:
